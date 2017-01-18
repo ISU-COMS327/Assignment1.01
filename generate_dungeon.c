b26d5432fad3d06c2c3c88a3260d7769cca9a091
Joseph McGovern <joseph.mcgovern@webfilings.com>
2017-01-18 15:50:39 -0600

    Add random number generator

--- generate_dungeon.c
+++ generate_dungeon.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
+#define HEIGHT 105
+#define WIDTH 160
+#define IMMUTABLE_ROCK -1
+#define ROCK 0
+#define ROOM 1
+#define CORRIDOR 2
+#define MIN_NUMBER_OF_ROOMS 10
+#define MIN_ROOM_WIDTH 7
+#define MAX_ROOM_WIDTH 15
+#define MIN_ROOM_HEIGHT 5
+#define MAX_ROOM_HEIGHT 15
+
+int board[HEIGHT][WIDTH] = {{0}};
+
+typedef struct {
+    int start_x;
+    int end_x;
+    int start_y;
+    int end_y;
+} tRoom;
+
+int random_int(int min_num, int max_num);
 
 int main(int argc, char *args[]) {
-    printf("Generating dungeon");
+    printf("Generating dungeon. Random number: %d\n", random_int(0, 50));
     return 0;
 }
+
+int random_int(int min_num, int max_num) {
+    int seed = time(NULL);
+    printf("Used seed: %d\n", seed);
+    max_num ++;
+    int delta = max_num - min_num;
+    srand(seed);
+    return (rand() % delta) + min_num;
+}
+
