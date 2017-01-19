e316241d43e326aa650e90379ade521c306a38b3
Joseph McGovern <joseph.mcgovern@webfilings.com>
2017-01-19 15:00:59 -0600

    Use random numbers to generate room locations

--- generate_dungeon.c
+++ generate_dungeon.c
@@ -24,7 +24,7 @@ struct Room {
     int end_y;
 };
 
-int random_int(int min_num, int max_num);
+int random_int(int min_num, int max_num, int add_to_seed);
 void initialize_immutable_rock();
 void print_board();
 void print_cell();
@@ -33,15 +33,18 @@ void dig_room(int index);
 struct Room rooms[MIN_NUMBER_OF_ROOMS];
 
 int main(int argc, char *args[]) {
-    printf("Generating dungeon. Random number: %d\n", random_int(0, 50));
+    printf("Generating dungeon. Random number: %d\n", random_int(0, 50, 0));
     initialize_immutable_rock();
     dig_rooms(MIN_NUMBER_OF_ROOMS);
     print_board();
     return 0;
 }
 
-int random_int(int min_num, int max_num) {
+int random_int(int min_num, int max_num, int add_to_seed) {
     int seed = time(NULL);
+    if (add_to_seed) {
+        seed += add_to_seed;
+    }
     printf("Used seed: %d\n", seed);
     max_num ++;
     int delta = max_num - min_num;
@@ -101,8 +104,21 @@ void dig_rooms(int number_of_rooms_to_dig) {
 void dig_room(int index) {
     // TODO write algorithm for determining room spaces
     printf("Digging a room for index %d\n", index);
-    rooms[index].start_x = 0;
-    rooms[index].start_y = 0;
-    rooms[index].end_x = 1;
-    rooms[index].end_y = 1;
+    int start_x = random_int(0, WIDTH, index);
+    int start_y = random_int(0, HEIGHT, index);
+    int room_height = random_int(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT, index);
+    int room_width = random_int(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH, index);
+    int end_y = start_y + room_height;
+    if (end_y >= HEIGHT) {
+        end_y = HEIGHT - 1;
+    }
+    int end_x = start_x + room_width;
+    if (end_x >= WIDTH) {
+        end_x = WIDTH - 1;
+    }
+    rooms[index].start_x = start_x;
+    rooms[index].start_y = start_y;
+    rooms[index].end_x = end_x;
+    rooms[index].end_y = end_y;
+    printf("ROOM DATA -- sX: %d, sY: %d, eX: %d, eY: %d, diffX: %d, diffY: %d\n", start_x, start_y, end_x, end_y, (end_x - start_x), (end_y - start_y));
 }
