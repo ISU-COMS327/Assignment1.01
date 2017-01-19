c0e08079c69625ecb6a0f9bf7aa60928f1cfad66
Joseph McGovern <joseph.mcgovern@webfilings.com>
2017-01-19 11:01:09 -0600

    Initialize immutable rock and print board

--- generate_dungeon.c
+++ generate_dungeon.c
@@ -25,9 +25,15 @@ typedef struct {
 } tRoom;
 
 int random_int(int min_num, int max_num);
+void initialize_immutable_rock();
+void print_board();
+void print_cell();
+void dig_rooms(int number_of_rooms_to_dig);
 
 int main(int argc, char *args[]) {
     printf("Generating dungeon. Random number: %d\n", random_int(0, 50));
+    initialize_immutable_rock();
+    print_board();
     return 0;
 }
 
@@ -40,3 +46,44 @@ int random_int(int min_num, int max_num) {
     return (rand() % delta) + min_num;
 }
 
+void initialize_immutable_rock() {
+    int y;
+    int x;
+    int max_x = WIDTH - 1;
+    int max_y = HEIGHT - 1;
+    for (y = 0; y < HEIGHT; y++) {
+        board[y][0] = IMMUTABLE_ROCK;
+        board[y][max_x] = IMMUTABLE_ROCK;
+    }
+    for (x = 0; x < WIDTH; x++) {
+        board[0][x] = IMMUTABLE_ROCK;
+        board[max_y][x] = IMMUTABLE_ROCK;
+    }
+}
+
+void print_board() {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            print_cell(board[y][x]);
+        }
+        printf("\n");
+    }
+}
+
+void print_cell(int cell) {
+    if (cell == IMMUTABLE_ROCK) {
+        printf("=");
+    }
+    else if (cell == ROCK) {
+        printf(" ");
+    }
+    else if (cell == ROOM) {
+        printf(".");
+    }
+    else if (cell == CORRIDOR) {
+        printf("#");
+    }
+    else {
+        printf("F");
+    }
+}
